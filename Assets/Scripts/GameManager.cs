858f404e6fffb9a66522de1af78b95298e20d7f3
Yedil-sama <sultanedil3@gmail.com>
2025-04-24 15:09:32 +0500

    Added Unity VFX & SFX & Meshes Assets

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,15 +1,43 @@
+using System;
+using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class GameManager : MonoBehaviour
 {
     public static GameManager Instance;
 
-    public int playerGold = 0;
+    [Header("Gold Management")]
+    private int playerGold = 0;
+    public int PlayerGold
+    {
+        get => playerGold;
+        set
+        {
+            if (value < 0)
+            {
+                value = 0;
+            }
+
+            playerGold = value;
+            OnChangeGold?.Invoke(playerGold);
+        }
+    }
+    public int goldIncome = 1;
+    public float goldIncomeInterval = 0.5f;
+    private float lastGoldTimer = 0f;
+
+    [Header("General")]
     public bool isAbilitySelected = false;
     public Player player;
 
     public float minSpeed = 0.1f;
     public float maxSpeed = 100f;
+
+    public event Action<int> OnChangeGold;
+
+    [SerializeField] private TMP_Text goldText;
+
     public void Awake()
     {
         if (Instance == null)
@@ -20,8 +48,23 @@ public class GameManager : MonoBehaviour
         {
             Destroy(Instance);
         }
+
+        OnChangeGold += UpdateGoldView;
+
         InitializePlayer();
     }
+
+    public void Update()
+    {
+        lastGoldTimer += Time.deltaTime;
+
+        if (lastGoldTimer >= goldIncomeInterval)
+        {
+            lastGoldTimer -= goldIncomeInterval;
+            PlayerGold += goldIncome;
+        }
+    }
+
     public void InitializePlayer()
     {
         player.Initialize();
@@ -35,6 +78,11 @@ public class GameManager : MonoBehaviour
         worldMousePos.y = player.transform.position.y;
 
         return worldMousePos;
+    }
+
+    private void UpdateGoldView(int amount)
+    {
+        goldText.text = amount.ToString();
     }
 
 }
